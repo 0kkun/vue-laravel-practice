ff423274786af62a437c9dc6b399b550f20badcf
0kkun <shinji.okumoto.01@gmail.com>
2020-12-26 17:35:40 +0900

    [Heroku] Herokuデプロイ用にDBの最大文字数を制限

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // DBの文字数の設定
+        // Herokuではvarchar型の文字数はデフォルトの255では大きすぎるため191に設定
+        Schema::defaultStringLength(191);
+
+        // アプリをhttpsに対応させる。本番環境の場合にhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
