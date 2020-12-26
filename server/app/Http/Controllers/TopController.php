3a20629a232e4e3eb6c2e40f15a9cbb15ae18d6f
0kkun <shinji.okumoto.01@gmail.com>
2020-12-26 17:14:53 +0900

    AgentのisMobileでPC判定できるかテスト

--- server/app/Http/Controllers/TopController.php
+++ server/app/Http/Controllers/TopController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Agent;
 
 class TopController extends Controller
 {
     public function index()
     {
-        return view('top.index');
+        Agent::isMobile() ? $devise_judge = 'mobile' : $devise_judge = 'PC';
+
+        return view('top.index', compact('devise_judge'));
     }
 }
--- server/resources/views/Top/index.blade.php
+++ server/resources/views/Top/index.blade.php
@@ -1,1 +1,2 @@
 Hello World!
+{{$devise_judge}}
