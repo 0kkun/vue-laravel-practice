853cceb6f3365964ac7c2a6022d6f99b40affb01
0kkun <shinji.okumoto.01@gmail.com>
2020-12-27 19:50:25 +0900

    header編集

--- server/resources/views/shared/header.blade.php
+++ server/resources/views/shared/header.blade.php
@@ -1,3 +1,20 @@
-<div>
-    Header
+<div class="container border-bottom p-3">
+    <div class="row">
+
+        <div class="col-sm-6">
+            <p class="text-left h3">Vue Laravel Practice</p>
+        </div>
+
+        <div class="col-sm-6">
+            @if(Auth::check())
+                <p class="text-right h4">ログインしてます</p>
+            @else
+                <div class="text-right h5">
+                    <div><a href="{{ route('login') }}">Login</a></div>
+                    <div class="pt-1"><a href="{{ route('register') }}">Register</a></div>
+                </div>
+            @endif
+        </div>
+
+    </div>
 </div>
