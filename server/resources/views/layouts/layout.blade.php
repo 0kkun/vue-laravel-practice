a1bdd075008e151f99abda87fae9e9a53bd566ec
0kkun <shinji.okumoto.01@gmail.com>
2020-12-27 19:50:45 +0900

    csrfの使用

--- server/resources/views/layouts/layout.blade.php
+++ server/resources/views/layouts/layout.blade.php
@@ -4,11 +4,11 @@
         <title>vue-laravel-tutrial</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
 
     <body>
-        
         @include('shared.header')
 
         <main>
@@ -16,8 +16,6 @@
         </main>
 
         @include('shared.footer')
-
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
-
 </html>
