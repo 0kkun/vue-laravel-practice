837d6237ea23e02064d3d906eb12615dd0dc7732
0kkun <shinji.okumoto.01@gmail.com>
2020-12-27 16:37:42 +0900

    index.blade.phpを編集

--- server/resources/views/Top/index.blade.php
+++ server/resources/views/Top/index.blade.php
@@ -1,2 +1,12 @@
-Hello World!
-{{-- {{$devise_judge}} --}}
+<!doctype html>
+<html lang="ja">
+    <head>
+        <title>TennisApp</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    <body>
+        Hello World!
+        {{ $devise_judge }}
+    </body>
+</html>
