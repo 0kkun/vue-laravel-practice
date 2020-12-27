8218ffb5ab7f2fa073633d7f3465fe996cd8ded4
0kkun <shinji.okumoto.01@gmail.com>
2020-12-27 19:05:14 +0900

    sassファイル読み込み

--- server/resources/views/layouts/layout.blade.php
+++ server/resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
         <title>vue-laravel-tutrial</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
 
     <body>
