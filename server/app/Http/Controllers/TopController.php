e5bb2cce7410529c2e49fbec30b452995b77190d
0kkun <shinji.okumoto.01@gmail.com>
2020-12-28 18:02:01 +0900

    TopControllerにアクション内容取得処理記述

--- server/app/Http/Controllers/TopController.php
+++ server/app/Http/Controllers/TopController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Agent;
+use App\Models\ActionTemplate;
+use App\Models\UsersAction;
+use Illuminate\Support\Facades\Auth;
 
 class TopController extends Controller
 {
@@ -19,8 +22,18 @@ class TopController extends Controller
 
     public function index()
     {
-        Agent::isMobile() ? $devise_judge = 'mobile' : $devise_judge = 'PC';
+        $actions = collect();
 
-        return view('Top/index', compact('devise_judge'));
+        Agent::isMobile() ? $devise_judge = 'mobile' : $devise_judge = 'PC';
+        $current_user = Auth::user();
+        $users_actions = UsersAction::get()->where('user_id', $current_user->id);
+
+        $action_templates = ActionTemplate::get();
+
+        foreach ($users_actions as $index => $users_action) {
+            $actions = $actions->concat( $action_templates->where('id', $users_action->action_template_id)->toArray() );
+        }
+
+        return view('Top/index', compact('devise_judge', 'actions'));
     }
 }
