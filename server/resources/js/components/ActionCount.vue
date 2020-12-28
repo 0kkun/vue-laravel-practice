7e8e23b6190978b10a38a47c0009bb87c6a36f1c
0kkun <shinji.okumoto.01@gmail.com>
2020-12-28 12:09:52 +0900

    ActionCountコンポーネント作成

---
+++ server/resources/js/components/ActionCount.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card mb-2">
+        <div class="card-body">
+            <h5 class="card-title">活動回数</h5>
+            <div class="card-text">
+                <span class="h4">{{ actionCount }} 回</span>
+                <button class="btn btn-primary" @click="increment">OK</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data : function () {
+            return {
+                actionCount: 0,
+            }
+        },
+        methods: {
+            increment() {
+                this.actionCount += 1;
+            }
+        }	
+    }
+</script>
+
+<style>
+</style>
