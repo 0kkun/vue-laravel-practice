4a9644d0526f2246b73991bd11f1940e98dc3291
0kkun <shinji.okumoto.01@gmail.com>
2020-12-31 17:52:55 +0900

    ゲストユーザー用のデータ流し込み

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'ゲスト',
+            'email' => 'guest@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
